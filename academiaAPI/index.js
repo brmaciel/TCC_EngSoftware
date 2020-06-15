3d62a0c2c3388687700f766938494c76f0ba2c70
Bruno Maciel <brunomaciel.xc4@gmail.com>
2020-06-15 16:22:30 -0300

    Limpeza de linhas desnecessarias do index.js

--- academiaAPI/index.js
+++ academiaAPI/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
-const app = express();         
 const bodyParser = require('body-parser');
+
+const app = express();
 const port = 3000; //porta padrão
-const mysql = require('mysql');
-const dbConection = require('./db_config');
 
 
 // Configurando o body parser para pegar POSTS
@@ -12,10 +11,6 @@ app.use(bodyParser.json());
 
 
 // Definindo as rotas
-const router = express.Router();
-app.use('/', router); // requisicoes que chegam na raiz sao mandadas para o router
-router.get('/', (req, res) => res.json({ message: 'Funcionando!' }));
-
 const InstutorRoute = require('./routes/instrutor_routes');
 const AulaRoute = require('./routes/aula_routes');
 const RelatorioRoute = require('./routes/relatorio_route');
@@ -23,8 +18,7 @@ const RelatorioRoute = require('./routes/relatorio_route');
 
 
 // Inicia o servidor
-app.listen(port);
-console.log('API funcionando!');
+app.listen(port, () => console.log('API funcionando!'));
 
 InstutorRoute(app);
 AulaRoute(app);
